8c0539e1234a777207c5d9ba5d5e322ec6a12c34
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-12-14 01:21:22 +0100

    Configure CORS for local development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,10 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+# config/initializers/cors.rb
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001'
+    resource '/api/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
